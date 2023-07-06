2eb9d0866da3539ea87f8486ab8be987b9963219
Guilhermecheng <guilherme.cheng@gmail.com>
2023-07-05 21:32:58 -0300

    fixing img imports

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,9 @@ export default {
     extend: {
       backgroundImage: {
         // 'cristo-wp': 'url(/cristo-wp.jpg)',
-        'cristo-wp': 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("cristo-wp.jpg")',
-        'animation-wp': 'linear-gradient( rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8) ), url("animation-wp.jpeg")',
-        'rio-wp': 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("rio.jpeg")',
+        'cristo-wp': 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./assets/cristo-wp.jpg")',
+        'animation-wp': 'linear-gradient( rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8) ), url("./assets/animation-wp.jpeg")',
+        'rio-wp': 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url("./assets/rio.jpeg")',
       },
     },
   },
